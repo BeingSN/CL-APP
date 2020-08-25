4f6f93c68fe9d15df19008636c5dd353dd4b9850
ruuuh20 <pkim2320@gmail.com>
2020-08-25 17:00:44 -0400

    fixing deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
@@ -34,6 +35,13 @@ app.get("/api/v1/standings", (req, res) => {
 
 // app.use("/api/v1/fixtures", fixtures);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(
